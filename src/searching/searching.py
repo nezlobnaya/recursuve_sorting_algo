7837e2f3e8ca7151c13387d440df1b15b254e8fc
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-07-08 18:52:34 -0400

    binary search recursive algo added and tested ok

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,16 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
-
+    if start > end:
+        return -1
+    else:
+        mid = (start + end) // 2
+        if target == arr[mid]:
+            return mid
+        elif target < arr[mid]:
+            return binary_search(arr, target, start, mid-1)
+        else:
+            return binary_search(arr, target, mid+1, end)
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +20,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
